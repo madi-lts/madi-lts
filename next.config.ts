ac77414c6b2dba8c2c9422454a3ae06eff957059
madi-lts <madi.j.anderson@gmail.com>
2025-01-15 10:35:15 -0500

    Add rehype-pretty-code for enhanced markdown styling and update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
+import nextMDX from "@next/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+  // See Options section below.
 };
 
-export default nextConfig;
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [[rehypePrettyCode, options]],
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = { reactStrictMode: true };
+
+export default withMDX(nextConfig);
+
+// export default nextConfig;
