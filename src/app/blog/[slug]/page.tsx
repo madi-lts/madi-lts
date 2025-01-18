9b30b1ba17ea02a00a039acf677fa4bf98ec7a62
madi-lts <madi.j.anderson@gmail.com>
2025-01-18 16:45:30 -0500

    Refactor CSS to update color and add responsive margin for .post class in blog post page Update package.json to add rehype-mathjax dependency Update page.tsx to include MathJax script and typeset equations Add mathjax.html include file for MathJax setup Remove unused MathJax related files Update posts.ts to add remark-math and rehype-mathjax plugins

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,13 +3,23 @@ import { Post, getPost } from '@/lib/posts';
 export default async function Page({params}: {params: Promise<{ slug: string }>}) {
   const id: string = (await params).slug
   const postData: Post = await getPost(id)
-  // const content: string = postData.unprocessedContent
+  // console.log(postData.contentHtml)
+
   return (
     <article className="prose prose-zinc post sm:{prose prose-zinc post}">
+      <script type='/text/javascript' id='MathJax-settings'>
+      </script>
+      <script type="text/javascript" id="MathJax-script" async
+          src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
+      </script>
       <h1 className="text-zinc-100">{postData.title}</h1>
       <h2 className="text-zinc-100">{new Date(postData.date).toLocaleDateString(
         'en-US', {month: 'long', day: 'numeric', year: 'numeric' })}</h2>
       <p className="sm:post" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <script>
+\       MathJax.typeset();
+      </script>
     </article>
+
   );
 }
--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -3,13 +3,17 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
-import rehypeSanitize from 'rehype-sanitize'
-import rehypeStringify from 'rehype-stringify'
+import {unified} from 'unified'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
-import {unified} from 'unified'
+import remarkMath from 'remark-math';
+import rehypeSanitize from 'rehype-sanitize'
+import rehypeStringify from 'rehype-stringify'
 import rehypePrettyCode from 'rehype-pretty-code';
 import rehypeAddClasses from 'rehype-add-classes';
+import rehypeMathjax from 'rehype-mathjax/browser'; 
+
+
 
 export interface Post {
     id: string;
@@ -19,6 +23,34 @@ export interface Post {
     unprocessedContent: string;
     contentHtml: string;
 }
+
+// const MathJax = {
+//     tex: {
+//         packages: ['base'],        // extensions to use
+//         inlineMath: [              // start/end delimiter pairs for in-line math
+//         ['$$', '$$']
+//         ],
+//         displayMath: [             // start/end delimiter pairs for display math
+//         ['\\[', '\\]']
+//         ],
+//         processEscapes: true,      // use \$ to produce a literal dollar sign
+//         processEnvironments: true, // process \begin{xxx}...\end{xxx} outside math mode
+//         processRefs: true,         // process \ref{...} outside of math mode
+//         digits: /^(?:[0-9]+(?:\{,\}[0-9]{3})*(?:\.[0-9]*)?|\.[0-9]+)/,
+//                                     // pattern for recognizing numbers
+//         tags: 'none',              // or 'ams' or 'all'
+//         tagSide: 'right',          // side for \tag macros
+//         tagIndent: '0.8em',        // amount to indent tags
+//         useLabelIds: true,         // use label name rather than tag for ids
+//         maxMacros: 10000,          // maximum number of macro substitutions per expression
+//         maxBuffer: 5 * 1024,       // maximum size for the internal TeX string (5K)
+//         // baseURL:                   // URL for use with links to tags (when there is a <base> tag in effect)
+//         //     (document.getElementsByTagName('base').length === 0) ?
+//         // //     '' : String(document.location).replace(/#.*$/, '')),
+//         // formatError:               // function called when TeX syntax errors occur
+//         //     (jax, err) => jax.formatError(err)
+//     }
+// };
 
 export async function getPost(id: string): Promise<Post> {
 
@@ -36,14 +68,20 @@ export async function getPost(id: string): Promise<Post> {
     // Use remark to convert markdown into HTML string
     const processedContent = await unified()
         .use(remarkParse)
+        .use(remarkMath)
         .use(remarkRehype)
         .use(rehypeSanitize)
-        .use(rehypeStringify)
+        .use(rehypeMathjax)
         .use(rehypePrettyCode, { theme: 'dracula', keepBackground: true })
-        .use(rehypeAddClasses, { '*': 'custom-class' })
+        .use(rehypeAddClasses, { 
+            '*': ['prose', 'prose-zinc', 'post', 'sm:prose', 'sm:prose-zinc', 'sm:post'],
+            'mjx-container': 'math-inline',
+            'mjx-container[display="true"]': 'math-display',
+        })
+        .use(rehypeStringify)
         .process(matterResult.content);
     const contentHtml = processedContent.toString();
-
+    
     // console.log(contentHtml);
 
     // Combine the data with the id
