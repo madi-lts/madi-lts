7fcb8f2279948923844df0fdf06413d18989b4f8
madi-lts <madi.j.anderson@gmail.com>
2025-01-18 07:57:53 -0500

    Refactor CSS to update color and add responsive margin for .post class

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -5,11 +5,11 @@ export default async function Page({params}: {params: Promise<{ slug: string }>}
   const postData: Post = await getPost(id)
   // const content: string = postData.unprocessedContent
   return (
-    <article className="prose prose-zinc dark:prose-invert post sm:post">
+    <article className="post sm:post">
       <h1>{postData.title}</h1>
       <h2>{new Date(postData.date).toLocaleDateString(
         'en-US', {month: 'long', day: 'numeric', year: 'numeric' })}</h2>
-      <p dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <p className="sm:post" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </article>
   );
 }
