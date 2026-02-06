5fe481f103eeaba13fe9d0ba63cff9316ea0b8b0
madi-lts <madisonja@pm.me>
2026-02-06 12:38:47 -0500

    Removed the title.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ export default async function Home() {
 
   return (
   <div>
-    <h1 className="text-center text-4xl">madi-lts</h1>
     {posts.map((post) => (
       <div key={post.id} className="mt-2">
         <Card key={post.id}>
